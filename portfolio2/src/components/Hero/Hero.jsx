6450694fb9c565e9e86368ec6cceb06bbd6a9805
Arunarchitect <intformbase@gmail.com>
2024-08-31 22:55:39 +0530

    added dynamic inputs

--- portfolio2/src/components/Hero/Hero.jsx
+++ portfolio2/src/components/Hero/Hero.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { useEffect, useState } from 'react';
+
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+        fetch('http://18.206.119.209/list/')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Fetched data:', data); // Log the data to the console
+                setData(data);
+            })
+            .catch(error => console.error('There was a problem with the fetch operation:', error));
+    }, []);
+
+     const profile = data.length > 0 ? data[0] : {};
+
+
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Anumol</h1>
+        <h1 className={styles.title}>Hi,{profile.name}</h1>
         <p className={styles.description}>
-          I'm an architect with 5 years of experience in Architecture. Reach out if you'd like to learn more!
+          {profile.about}
         </p>
         <a href="mailto:anumolregi998@gmail.com" className={styles.contactBtn}>
           Contact Me
